568d243e1d31b1acbb3ddcebe7e0a6e3c63b34d9
aravinthankumar <aravinthankumara@gmail.com>
2022-05-05 16:43:26 +0530

    test

--- src/test/java/org/date21/Date21.java
+++ src/test/java/org/date21/Date21.java
@@ -9,51 +9,47 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Date21 extends BaseClass  {
-	
+public class Date21 extends BaseClass {
+
 	@BeforeClass
 	private void beforeClass() {
 		System.out.println("before class");
-		
+		System.out.println("before");
+
 	}
-	
+
 	@AfterClass
 	private void afterClass() {
-		//driver.quit();
-      System.out.println("after class");
-		
+		// driver.quit();
+		System.out.println("after class");
+
 	}
-	
+
 	@BeforeMethod
 	private void beforeMethod() {
-		Date d =new Date();
+		Date d = new Date();
 		System.out.println(d);
 		System.out.println("before Method");
-		
+
 	}
-	
+
 	@AfterMethod
 	private void afterMethod() {
-		Date d =new Date();
+		Date d = new Date();
 		System.out.println(d);
 		System.out.println("after method");
-		
+
 	}
-	
-	
-	
+
 	@Test(invocationCount = 5)
 	private void test2() {
- System.out.println("test case 2");
+		System.out.println("test case 2");
 	}
-	
-	
+
 	@Test
 	private void test3() {
 		System.out.println("new");
- System.out.println("test hi case 3");
+		System.out.println("test hi case 3");
 	}
-	
-	
 
 }
